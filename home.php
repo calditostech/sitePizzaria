676abf696e088eab9e95ee046c2e04c21095a292
Ricardo Moreira <calditos1988@outook.com>
2021-04-17 00:35:49 -0300

    criação da header

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Pizzaria Mazza</title>
+</head>
+<body>
+
+<div class="grid-container">
+  <div class="header">
+    <div class="imagem">
+        <img src = "img/logo-pizza.png" class="logo">
+   </div>
+   <div class = "menu">
+     <a href="">Sobre Nós</a>
+     <a href="">Catalogos</a> 
+     <a href="">Contatos</a>      
+  </div>
+  <div class="main">
+      
+  </div>  
+  <div class="footer">
+      
+  </div>
+</div>
+</body>
+</html>
