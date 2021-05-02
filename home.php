91ccd31d988d6f01ad662f732b60a09b02d1ae4a
Ricardo Moreira <calditos1988@outlook.com>
2021-05-02 15:07:15 -0300

    criando os cards

--- home.php
+++ home.php
@@ -1,46 +1,112 @@
 <!DOCTYPE html>
 <html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Pizzaria Mazza</title>
-</head>
+    <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+          <link rel="stylesheet" href="css/style.css">
+              <title>Pizzaria Mazza</title>
+    </head>
 <body>
 
 <div class="grid-container">
-  <div class="header">
-    <div class="imagem">
-        <img src = "img/logo-pizza.png" class="logo">
-   </div>
-   <div class = "menu">
-     <a href="">Sobre Nós</a>
-     <a href="">Catalogos</a> 
-     <a href="">Contatos</a>      
-  </div>
+      <div class="header">
+        <div class="imagem">
+            <img src = "img/logo-pizza.png" class="logo">
+      </div>
+      <div class = "menu">
+        <a href="">Sobre Nós</a>
+        <a href="">Catalogos</a> 
+        <a href="">Contatos</a>      
+      </div>
+
+
   <div class="main">
-  <ul class="slider">
-    <li>
-          <input type="radio" id="slide1" name="slide" checked>
-          <label for="slide1"></label>
-          <img src="img/slide2.jpg" />
-    </li>
-   <!-- <li>
-          <input type="radio" id="slide2" name="slide">
-          <label for="slide2"></label>
-          <img src="img/slide2.jpg" />
-    </li>
-    <li>
-          <input type="radio" id="slide3" name="slide">
-          <label for="slide3"></label>
-          <img src="img/slide3.jpg" />
-    </li> -->
-</ul>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/slide1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/slide2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/slide3.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+  </div>
+
+  <div class="card-pizza">
+        <h5 style="text-align: center;">CONHEÇA NOSSO CARDAPIO</h5>
+        <div class="card-group">
+          <div class="card">
+            <img src="img/pexels-andy-kuzma.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pizza Kuzma</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/pexels-cottonbro.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pizza Cottonbro</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/pexels-engin-akyurt.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pizza Akyurt</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card-group">
+          <div class="card">
+            <img src="img/pexels-jonathan-petersson.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pizza Petterson</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/pexels-cottonbroa.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pizza Conttonbroa</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="img/pexels-rehan-verma.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pizza Rehan</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+  </div>
   </div>  
   <div class="footer">
-      
+  <div class="container">
+  <div class="row">
+    <div class="col align-self-start">
+      One of three columns
+    </div>
+    <div class="col align-self-center">
+      One of three columns
+    </div>
+    <div class="col align-self-end">
+      One of three columns
+    </div>
   </div>
 </div>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 </html>
